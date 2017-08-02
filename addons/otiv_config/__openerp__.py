dd455d67988c579431cbf52813345df9ccd15186
Michael Karrer <michaelkarrer81@gmail.com>
2017-08-03 00:31:47 +0200

    [ADD] __openerp__.py for the default instance addon

---
+++ addons/otiv_config/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'otiv_config',
+    'summary': '''FS-Online otiv instance configuration''',
+    'description': '''
+FS-Online Instance Configuration
+================================
+
+Customer configuration for the instance otiv
+
+- Default settings
+- View modifications
+- CSS
+- Translations
+    ''',
+    'author': 'Michael Karrer (michael.karrer@datadialog.net), DataDialog',
+    'version': '1.0',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'fsonline',
+    ],
+    'data': [],
+}
+
+    
